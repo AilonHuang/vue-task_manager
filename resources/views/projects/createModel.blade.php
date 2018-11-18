c8c92bc77b1bc619663a3ed9f17acecd78173931
529678737@qq.com <gy5845201019>
2018-11-18 10:39:30 +0800

    使用 laravel collective form 新建项目表单

--- resources/views/projects/createModel.blade.php
+++ resources/views/projects/createModel.blade.php
@@ -4,22 +4,32 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="createProjectModel" tabindex="-1" role="dialog" aria-labelledby="createProjectModelLabel" aria-hidden="true">
+<div class="modal fade" id="createProjectModel" tabindex="-1" role="dialog" aria-labelledby="createProjectModelLabel"
+     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="createProjectModelLabel">Modal title</h5>
+                <h5 class="modal-title" id="createProjectModelLabel">新建项目</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+            {!! Form::open() !!}
             <div class="modal-body">
-                ...
+
+                <div class="form-group">
+                    {!! Form::label('name', '项目名称：') !!}
+                    {!! Form::text('name', '', ['class' => 'form-control']) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('thumbnail', '项目缩略图：') !!}
+                    {!! Form::file('thumbnail', ['class' => 'form-control-file']) !!}
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                {!! Form::submit('新建项目', ['class' => 'btn btn-primary']) !!}
             </div>
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
