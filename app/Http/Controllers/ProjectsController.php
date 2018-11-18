cda623b30a30a7608c064a0f21c3154e792f4bc2
529678737@qq.com <gy5845201019>
2018-11-18 12:11:13 +0800

    文件上传与保存相关操作

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,10 +6,22 @@ use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
+
+
         $request->user()->projects()->create([
             'name' => $request->name,
-            'thumbnail' => $request->thumbnail
+            'thumbnail' => $this->thumb($request)
         ]);
+    }
+
+    public function thumb($request)
+    {
+        $path = null;
+        if ($request->hasFile('thumbnail')) {
+            $path = $request->thumbnail->store('public/thumbs');
+        }
+        return $path;
     }
 }
