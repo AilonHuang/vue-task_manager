40e3d8a8370c0f2bba1a3f9da824f99dd8f56ed0
529678737@qq.com <gy5845201019>
2018-11-18 18:40:48 +0800

    代码重构：SOC 设计原则与 repository class

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,30 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateProjectRequest;
+use App\Repositories\ProjectsRepository;
 use Image;
-use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function store(Request $request)
+    protected $repository;
+    public function __construct(ProjectsRepository $repository)
     {
-        $request->user()->projects()->create([
-            'name' => $request->name,
-            'thumbnail' => $this->thumb($request)
-        ]);
+        $this->repository = $repository;
     }
 
-    public function thumb($request)
+    public function store(CreateProjectRequest $request)
     {
-        if ($request->hasFile('thumbnail')) {
-            $thumb = $request->thumbnail;
-            $name = $thumb->hashName();
-            // 保存图片
-            $request->thumbnail->storeAs('public/thumbs/original', $name);
-            $path = storage_path('app/public/thumbs/cropped/' . $name);
-            // 处理图片
-            Image::make($thumb)->resize(200, 90)->save($path);
-            return $name;
-        }
+        $this->repository->create($request);
     }
 }
