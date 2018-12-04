9df83f6342060639e749f7bcd174646647465b3c
AilonHuang <529678737@qq.com>
2018-12-04 12:58:23 +0000

    已完成和待完成任务的数目显示

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,11 +5,17 @@
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item">
                 <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo" role="tab" aria-controls="todo"
-                   aria-selected="true">代办事项</a>
+                   aria-selected="true">
+                    代办事项
+                    <span class="badge badge-pill badge-danger">{{count($todos)}}</span>
+                </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" id="done-tab" data-toggle="tab" href="#done" role="tab" aria-controls="done"
-                   aria-selected="false">已完成</a>
+                   aria-selected="false">
+                    已完成
+                    <span class="badge badge-pill badge-success">{{count($dones)}}</span>
+                </a>
             </li>
         </ul>
         <div class="tab-content" id="myTabContent">
@@ -18,7 +24,10 @@
                     <table class="table table-striped">
                         @foreach($todos as $task)
                             <tr>
-                                <td class="col-9 pl-5">{{$task->name}}</td>
+                                <td class="col-9 pl-5">
+                                    <span class="badge badge-secondary mr-3">{{$task->updated_at->diffForHumans()}}</span>
+                                    {{$task->name}}
+                                </td>
                                 <td>@include('tasks._checkForm')</td>
                                 <td>@include('tasks._editModal')</td>
                                 <td>@include('tasks._deleteForm')</td>
